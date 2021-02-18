be73a999a6f17aabb14cc390f2a095d95b9fc5f9
Omar AlSadi <66041816+omar-alsadi@users.noreply.github.com>
2021-02-17 20:12:39 -0500

    solution for Plogg challenge

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>OMAR ALSADI</h2>
+    <span>
+      Create a VueJs Component that has two input fields and displays the sum of
+      the values in them.</span
+    >
+    <Inputs />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Inputs from "./components/Inputs";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Inputs,
+  },
+};
 </script>
 
 <style>
@@ -23,6 +27,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 600px;
+  margin: 3rem auto;
+}
+
+h2 {
+  color: #42b983;
 }
 </style>
---
+++ src/components/Inputs.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <label>Value 1 :</label>
+    <input
+      type="number"
+      placeholder="Enter the value"
+      v-model="value1"
+      required
+    />
+    <label>Value 2 :</label>
+    <input
+      type="number"
+      placeholder="Enter the value"
+      v-model="value2"
+      required
+    />
+    <label>Sum :</label>
+    <h1>{{ sum(value1, value2) }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Inputs",
+  data() {
+    return {
+      value1: 0,
+      value2: 0,
+    };
+  },
+  methods: {
+    sum: function (value1, value2) {
+      if (value1 !== "" && value2 !== "") {
+        return parseInt(value1) + parseInt(value2);
+      } else {
+        return "Please put value in the empty input";
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  padding: 2rem;
+  max-width: 600px;
+}
+
+input {
+  padding: 1rem;
+  margin: 1rem 0;
+  border-radius: 10px;
+}
+</style>
